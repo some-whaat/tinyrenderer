28b9879ed211c21c191984cda9a4a5be74e48dcc
zhenya shur <zhenforshur@gmail.com>
2025-11-21 15:48:14 +0300

    linesss

--- main.cpp
+++ main.cpp
@@ -6,14 +6,13 @@ constexpr TGAColor red     = {  0,   0, 255, 255};
 constexpr TGAColor blue    = {255, 128,  64, 255};
 constexpr TGAColor yellow  = {  0, 200, 255, 255};
 
-void line(float ax, float ay, float bx, float by, TGAImage framebuffer) {
+void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color = white) {
     for (float t = 0.; t <= 1.; t += 0.02) {
-        float x_coord = ax * (1. - t) + bx * t;
-        float y_coord = ay * (1. - t) + by * t;
-        framebuffer.set(x_coord, y_coord, white);
+        int x_coord = ax * (1. - t) + bx * t;
+        int y_coord = ay * (1. - t) + by * t;
+        framebuffer.set(x_coord, y_coord, color);
     }
 }
-
 
 int main(int argc, char** argv) {
     constexpr int width  = 64;
@@ -24,7 +23,10 @@ int main(int argc, char** argv) {
     int bx = 12, by = 37;
     int cx = 62, cy = 53;
 
-    line(ax, ay, bx, by, framebuffer);
+    line(ax, ay, bx, by, framebuffer, blue);
+    line(cx, cy, bx, by, framebuffer, green);
+    line(cx, cy, ax, ay, framebuffer, yellow);
+    line(ax, ay, cx, cy, framebuffer, red);
 
     framebuffer.set(ax, ay, white);
     framebuffer.set(bx, by, white);
