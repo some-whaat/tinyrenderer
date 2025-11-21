d1adb8a668ec8147e6ded5a68316ba30d42a4f05
zhen <zhenforshur@gmail.com>
2025-11-21 17:25:13 +0300

    line func

--- main.cpp
+++ main.cpp
@@ -6,6 +6,15 @@ constexpr TGAColor red     = {  0,   0, 255, 255};
 constexpr TGAColor blue    = {255, 128,  64, 255};
 constexpr TGAColor yellow  = {  0, 200, 255, 255};
 
+void line(float ax, float ay, float bx, float by, TGAImage framebuffer) {
+    for (float t = 0.; t <= 1.; t += 0.02) {
+        float x_coord = ax * (1. - t) + bx * t;
+        float y_coord = ay * (1. - t) + by * t;
+        framebuffer.set(x_coord, y_coord, white);
+    }
+}
+
+
 int main(int argc, char** argv) {
     constexpr int width  = 64;
     constexpr int height = 64;
@@ -14,6 +23,8 @@ int main(int argc, char** argv) {
     int ax =  7, ay =  3;
     int bx = 12, by = 37;
     int cx = 62, cy = 53;
+
+    line(ax, ay, bx, by, framebuffer);
 
     framebuffer.set(ax, ay, white);
     framebuffer.set(bx, by, white);
